bb2d97940a39c10f1c622c59a9627787782e87ea
fool metuljakut <foolmetuljakut@gmail.com>
2025-12-14 14:05:54 +0100

    added home-button baseclass
    add-drinks, find-drinks and go-to-drinks should inherit from home-button

--- gui/home/home_widgets/go_to_drinks_button.py
+++ gui/home/home_widgets/go_to_drinks_button.py
@@ -1,36 +1,24 @@
 from PySide6.QtGui import Qt, QPixmap
 from PySide6.QtWidgets import QPushButton, QSizePolicy, QLabel
+from .home_button import HomeButton
 from core.utility import Utility
 
 
-class GotoDrinksButton(QPushButton):
+class GotoDrinksButton(HomeButton):
     def __init__(self, path, goto_drinks_callback):
-        super().__init__()
-        self._path = path
-        self._image_label = QLabel(self)
+        super().__init__(
+            path, 
+            styling="background-color: transparent; border: none;", 
+            display_image_filename="go_to_drinks.png"
+        )
         self._callback = goto_drinks_callback
 
-    def initialize(self):
-        self._set_style()
-        self._initialize_image_label()
-
-    def _set_style(self):
-        self.setStyleSheet("background-color: transparent; border: none;")
-        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-
     def _initialize_image_label(self):
-        self._image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self._image_label.setScaledContents(True)
-
-        pixmap = QPixmap(Utility.get_image_path("go_to_drinks.png", self._path))
-        self._image_label.setPixmap(pixmap)
+        super()._initialize_image_label()
 
         if self._callback:
             self.clicked.connect(self._callback)
 
-    def resizeEvent(self, event):
-        self._image_label.setFixedSize(event.size()) #damit wird das Icon so groß wie der Button gesetzt
-        super().resizeEvent(event)
 
 
 
---
+++ gui/home/home_widgets/home_button.py
@@ -0,0 +1,32 @@
+from PySide6.QtGui import Qt, QPixmap
+from PySide6.QtWidgets import QPushButton, QSizePolicy, QLabel
+
+from core.utility import Utility
+
+
+class HomeButton(QPushButton):
+    def __init__(self, path, styling, display_image_filename):
+        super().__init__()
+        self._path = path
+        self._image_label = QLabel(self)
+        self._styling = styling
+        self._display_image_filename = display_image_filename
+
+    def initialize(self):
+        self._set_style()
+        self._initialize_image_label()
+
+    def _set_style(self):
+        self.setStyleSheet(self._styling)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+    def _initialize_image_label(self):
+        self._image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self._image_label.setScaledContents(True)
+
+        pixmap = QPixmap(Utility.get_image_path(self._display_image_filename, self._path))
+        self._image_label.setPixmap(pixmap)
+
+    def resizeEvent(self, event):
+        self._image_label.setFixedSize(event.size()) #damit wird das Icon so groß wie der Button gesetzt
+        super().resizeEvent(event)
